4a3488f5692ac6aa20928ae4087239c22e0c17ad
mahmudrkb <almahmudrkb98@gmail.com>
2024-12-13 00:19:20 +0600

    get details

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 //middle ware
 
@@ -38,6 +38,12 @@ async function run() {
   app.get("/jobs",async(req,res)=>{
     const cursor=jobCollections.find();
     const result=await cursor.toArray();
+    res.send(result)
+  })
+  app.get("/jobs/:id",async(req,res)=>{
+    const id=req.params.id
+    const query={ _id: new ObjectId(id)}
+    const result=await jobCollections.findOne(query)
     res.send(result)
   })
 
