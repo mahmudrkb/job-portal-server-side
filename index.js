ce89cdeb95fa4bbc143b9c32711551010c5a962c
mahmudrkb <almahmudrkb98@gmail.com>
2024-12-12 21:30:09 +0600

    api data create

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
+
+//middle ware
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kw4xi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+
+  const jobCollections=client.db("job-portal").collection("job")
+
+  app.get("/jobs",async(req,res)=>{
+    const cursor=jobCollections.find();
+    const result=await cursor.toArray();
+    res.send(result)
+  })
+
+
+
+
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("job is falling form the sky");
+});
+app.listen(port, () => {
+  console.log(`"server is running" ${port} `);
+});
